8fc9c9ad3d8456073c02a265b41438b98cf0e724
Gerem66 <geremy.lecaplain66@gmail.com>
2023-01-19 22:40:13 +0000

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # LyricsSync
+
+## Packages
+- [Pydub](https://github.com/jiaaro/pydub)
+- [PocketSphinx](https://github.com/cmusphinx/pocketsphinx)
+- [FuzzyWuzzy](https://github.com/seatgeek/fuzzywuzzy)
---
+++ lyrics-sync.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+import os
+import pydub
+from difflib import SequenceMatcher
+
+import pocketsphinx             # Audio recognition
+from fuzzywuzzy import fuzz     # Text phonetic comparison
+
+class LyricsSync:
+    tempFolder = './parts/'
+    step = .5
+
+    def __init__(self, filename: str, lyrics: list[str]):
+        self.filename = filename
+        self.lyrics = lyrics
+
+        self.tempFolder += os.path.basename(filename) + '/'
+        self.ClearTempFolder()
+        if not os.path.exists(self.tempFolder):
+            os.mkdir(self.tempFolder)
+
+        self.audio = pydub.AudioSegment.from_file(filename, format='wav')
+        self.data = {}
+
+    def ClearTempFolder(self):
+        if not os.path.exists(self.tempFolder): return
+
+        for file in os.listdir(self.tempFolder):
+            os.remove(self.tempFolder + file)
+        os.rmdir(self.tempFolder)
+
+    def SplitFile(self):
+        audioLength = len(self.audio)
+        for i in range(0, audioLength, int(self.step * 1000)):
+            newBasename = 'audio_part_{}.wav'.format(round(i/1000, 2))
+            newFilename = self.tempFolder + newBasename
+            audio_part = self.audio[i:i+int(self.step * 1000)]
+            audio_part.export(newFilename, format='wav')
+
+    def Recognize(self):
+        self.data = {}
+        for file in sorted(os.listdir(self.tempFolder)):
+            sphinx = pocketsphinx.AudioFile(audio_file=self.tempFolder + file)
+            recognitionText = ''
+            for reco in sphinx:
+                recognitionText += str(reco)
+            self.data[file] = {}
+            for line in self.lyrics:
+                self.data[file][line] = LyricsSync.GetRatio(line, recognitionText)
+
+    def GetRatio(reference, line):
+        if len(line) + 5 > len(reference):
+            line = line[:len(reference) + 4]
+        ratio1 = fuzz.token_sort_ratio(reference, line)
+
+        sequence = SequenceMatcher(a=reference, b=line).ratio()
+        ratio2 = round(sequence * 100)
+
+        return { 'ratio1': ratio1, 'ratio2': ratio2 }
+
+    def Analyze(self):
+        pass
+
+    def Save(self):
+        with open(self.tempFolder + 'data.txt', 'w') as f:
+            # Write header
+            f.write('file,')
+            f.write(','.join(self.lyrics))
+            f.write('\n')
+
+            # Write data
+            for file in self.data.keys():
+                f.write(file)
+                for line in self.lyrics:
+                    ratioText = '{}|{}'.format(self.data[file][line]['ratio1'], self.data[file][line]['ratio2'])
+                    f.write(',' + ratioText)
+                f.write('\n')
+
+# Get lyrics from file
+with open('lyrics.txt', 'r') as f:
+    lines = f.readlines()
+filename = './juice-voice.wav'
+
+lyricsSync = LyricsSync(filename, lines)
+lyricsSync.step = .2
+
+lyricsSync.SplitFile()
+lyricsSync.Recognize()
+lyricsSync.Analyze()
+
+#print(lyricsSync.data)
+lyricsSync.Save()
